d8560c9c6d06d003c566b6b75d353af8c3c54f79
marta meshveliani <mmesh21@freeuni.edu.ge>
2025-06-18 02:03:48 +0400

    Resolve merge conflicts and update project configuration
    - Clean up pom.xml and web.xml merge conflicts
    - Add WebStore package with ShoppingCartServlet
    - Update Maven dependencies including Gson
    - Remove AccountManagerTest.java
    - Update solar_system.html

---
+++ src/main/java/WebStore/ShoppingCartServlet.java
@@ -0,0 +1,19 @@
+package WebStore;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ShoppingCartServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO: Implement shopping cart functionality
+        response.getWriter().println("Shopping cart functionality not implemented yet");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO: Implement shopping cart functionality
+        response.getWriter().println("Shopping cart functionality not implemented yet");
+    }
+}
